122e5d4c7e6a12fc7f3e5fce1a7e1895609a5e84
Dmitriy Narishkin <97532271+snow625@users.noreply.github.com>
2022-05-25 23:07:28 +0300

    Start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
-console.log('Start');
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const lastSeconsBeforeStopIframe = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(lastSeconsBeforeStopIframe)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(function (event) {
+    localStorage.setItem('videoplayer-current-time', `${event.seconds}`);
+  }, 1000),
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+const inputEmailEl = formEl.querySelector('input');
+const textareaMessageEl = formEl.querySelector('textarea');
+
+formEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+  if (inputEmailEl.value && textareaMessageEl.value) {
+    console.log({
+      [inputEmailEl.name]: inputEmailEl.value,
+      [textareaMessageEl.name]: textareaMessageEl.value,
+    });
+    formEl.reset();
+    localStorage.removeItem('feedback-form-state');
+  }
+});
+
+formEl.addEventListener('input', throttle(onInputPress, 500));
+
+const inputObj = {
+  email: '',
+  message: '',
+};
+
+function onInputPress(event) {
+  inputObj[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputObj));
+}
+
+const saveDateUserStr = localStorage.getItem('feedback-form-state');
+const saveDateUseObj = JSON.parse(saveDateUserStr);
+
+if (saveDateUseObj) {
+  inputObj.email = saveDateUseObj.email;
+  inputObj.message = saveDateUseObj.message;
+
+  inputEmailEl.value = saveDateUseObj.email;
+  textareaMessageEl.value = saveDateUseObj.message;
+}
